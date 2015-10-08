b0082e0b341ad3967b96eebd089d275d9dc89c3d
Michal Špaček <tupinek@gmail.com>
2015-10-08 09:29:48 +0200

    Add EXAMPLE4 to doc.

--- NizhnyNovgorod.pm
+++ NizhnyNovgorod.pm
@@ -196,6 +196,29 @@ For more information about NizhnyNovgorod Map, click L<here|https://en.wikipedia
 
 =end html
 
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::NizhnyNovgorod;
+
+ # Object.
+ my $obj = Map::Tube::NizhnyNovgorod->new;
+
+ # Get lines.
+ my $lines_ar = $obj->get_lines;
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } sort @{$lines_ar};
+
+ # Output:
+ # Автозаводская линия
+ # Сормовская линия
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
