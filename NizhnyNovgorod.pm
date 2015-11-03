22f77012e7489da79675990c24c1c13222395799
Michal Špaček <tupinek@gmail.com>
2015-11-03 09:31:09 +0100

    Add EXAMPLE5 section to doc.

--- NizhnyNovgorod.pm
+++ NizhnyNovgorod.pm
@@ -219,6 +219,44 @@ For more information about NizhnyNovgorod Map, click L<here|https://en.wikipedia
  # Автозаводская линия
  # Сормовская линия
 
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(decode_utf8 encode_utf8);
+ use Map::Tube::NizhnyNovgorod;
+
+ # Arguments.
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 line\n";
+         exit 1;
+ }
+ my $line = decode_utf8($ARGV[0]);
+
+ # Object.
+ my $obj = Map::Tube::NizhnyNovgorod->new;
+
+ # Get stations for line.
+ my $stations_ar = $obj->get_stations($line);
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } @{$stations_ar};
+
+ # Output:
+ # Usage: __PROG__ line
+
+ # Output with 'foo' argument.
+ # Map::Tube::get_stations(): ERROR: Invalid Line Name [foo]. (status: 105) file __PROG__ on line __LINE__
+
+ # Output with 'Сормовская линия' argument.
+ # Московская
+ # Канавинская
+ # Бурнаковская
+ # Буревестник
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
